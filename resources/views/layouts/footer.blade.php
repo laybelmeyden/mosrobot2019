e4116874f7e4017accb9a5702383d3ec05627bd6
laybelmeyden <trycollens@gmail.com>
2019-11-25 21:09:29 +0300

    ad

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -40,4 +40,23 @@
 </div>
 </div>
 </footer>
-<div class="footer_end"></div>
+<div class="footer_end"><!-- Yandex.Metrika informer -->
+<a href="https://metrika.yandex.ru/stat/?id=56392237&amp;from=informer"
+target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/56392237/1_0_20284DFF_00082DFF_0_pageviews"
+style="width:80px; height:15px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры)" class="ym-advanced-informer" data-cid="56392237" data-lang="ru" /></a>
+<!-- /Yandex.Metrika informer -->
+
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(56392237, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/56392237" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter --></div>
