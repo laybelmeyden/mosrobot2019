4cf1ab3ab357fc559604ebeeebbdb20058308391
laybelmeyden <trycollens@gmail.com>
2019-11-27 17:15:34 +0300

    ds

--- resources/views/pages/more.blade.php
+++ resources/views/pages/more.blade.php
@@ -16,11 +16,11 @@
   </div>
   <p style="color:red; font-size: 24px;
     padding-top: 10px;
-    font-weight: bold; width: 100%;" class="plos">РЕГИСТРАЦИЯ ОТКРЫТА ДО 29.11
-    </p>
+    font-weight: bold; width: 100%;" class="plos">РЕГИСТРАЦИЯ ОТКРЫТА ДО 29.11<br>
     <span style="color:red; font-size: 24px;
     padding-top: 10px;
-    font-weight: bold; width: 100%;font-size:14px;">*количество мест ограничено и регистрация может закрыться раньше указанной даты</span>
+    font-weight: bold; width: 100%;">*количество мест ограничено и регистрация может закрыться раньше указанной даты</span>
+    </p>
   </div>
 </div>
 </section>
